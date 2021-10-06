56058aabdde0fb835ae57d371e717dabca7c7eb8
VarunPotti <potti.varun07@gmail.com>
2021-10-06 14:15:01 +0530

    fix(web): gave functionality for button

--- web/src/lib/Unauth/Login/components/Button.svelte
+++ web/src/lib/Unauth/Login/components/Button.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import axios from 'axios';
 	import Google from '../../../assets/Login/Google.svg';
 	import Twitter from '../../../assets/Login/Twitter.svg';
 	import Github from '../../../assets/Login/Github.svg';
@@ -15,11 +16,20 @@
 	} else {
 		icon = Linkedin;
 	}
+
+	const onClickHandler = () => {
+		axios.get(`http://localhost:8000/api/v1/auth/login?provider=${provider}`).then((val) => {
+			let { url } = val.data;
+
+			window.location.replace(url);
+		});
+	};
 </script>
 
-<div
-	class="w-full h-full bg-acrylic-100 px-4 py-3 flex items-center justify-center gap-3 rounded-lg cursor-pointer hover:bg-acrylic-200 duration-200 font-bold "
+<button
+	on:click={onClickHandler}
+	class="w-full h-full bg-acrylic-100 px-4 py-3 flex items-center justify-center gap-3 rounded-lg cursor-pointer hover:bg-acrylic-200 duration-200 font-bold focus:border-iris-600 focus:ring focus:outline-none"
 >
 	<img src={icon} alt="" width="20em" />
 	{providerName}
-</div>
+</button>
