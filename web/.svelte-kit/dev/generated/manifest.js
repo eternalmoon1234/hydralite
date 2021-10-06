1bc621b7ee24be9425e23998abff99781ed28645
VarunPotti <potti.varun07@gmail.com>
2021-10-06 15:23:20 +0530

    feat(web): Completed auth

--- web/.svelte-kit/dev/generated/manifest.js
+++ web/.svelte-kit/dev/generated/manifest.js
@@ -1,14 +1,18 @@
 const c = [
 	() => import('../../../src/routes/__layout.svelte'),
 	() => import('../components/error.svelte'),
-	() => import('../../../src/routes/index.svelte')
+	() => import('../../../src/routes/index.svelte'),
+	() => import('../../../src/routes/auth/[provider].svelte')
 ];
 
 const d = decodeURIComponent;
 
 export const routes = [
 	// src/routes/index.svelte
-	[/^\/$/, [c[0], c[2]], [c[1]]]
+	[/^\/$/, [c[0], c[2]], [c[1]]],
+
+	// src/routes/auth/[provider].svelte
+	[/^\/auth\/([^/]+?)\/?$/, [c[0], c[3]], [c[1]], (m) => ({ provider: d(m[1]) })]
 ];
 
 // we import the root layout/error components eagerly, so that
---
+++ web/src/routes/auth/[provider].svelte
@@ -0,0 +1,26 @@
+<script lang="ts" context="module">
+	export async function load({ page }) {
+		const {
+			params: { provider }
+		} = page;
+
+		return { props: { provider } };
+	}
+</script>
+
+<script>
+	import axios from 'axios';
+	import { onMount } from 'svelte';
+	export let provider;
+	onMount(() => {
+		axios
+			.get(`http://localhost:8000/api/v1/auth/${provider}${window.location.search}`)
+			.then((val) => console.log(val.data));
+	});
+</script>
+
+<div
+	class="bg-white h-screen w-screen dark:bg-acrylic-700 flex items-center justify-center absolute flex-col dark:text-white text-black select-none"
+>
+	<img src="/logo/logo.svg" alt="Hydralite logo" width="8%" class="motion-safe:animate-pulse" />
+</div>
