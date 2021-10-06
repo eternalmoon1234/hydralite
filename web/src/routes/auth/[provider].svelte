62ec52692d2b6b4d93029259327bc01334e81373
VarunPotti <potti.varun07@gmail.com>
2021-10-06 19:20:28 +0530

    fix(web): Added error handling + use env var url

--- web/src/routes/auth/[provider].svelte
+++ web/src/routes/auth/[provider].svelte
@@ -8,14 +8,28 @@
 	}
 </script>
 
-<script>
+<script lang="ts">
 	import axios from 'axios';
 	import { onMount } from 'svelte';
-	export let provider;
+	export let provider: string;
+	let errorMSG: string = 'Logging in';
 	onMount(() => {
 		axios
-			.get(`http://localhost:8000/api/v1/auth/${provider}${window.location.search}`)
-			.then((val) => console.log(val.data));
+			.get(`${import.meta.env.VITE_WEB_URL}auth/${provider}${window.location.search}`)
+			.then((val) => {
+				const resp = val.data;
+				let { error } = resp;
+				if (error === undefined) {
+					let { accessToken, refreshToken } = resp;
+					localStorage.setItem('accessToken', accessToken);
+					localStorage.setItem('refreshToken', refreshToken);
+				} else {
+					errorMSG = 'An unexpected error occured redirecting you to home page in 5 seconds';
+					setTimeout(function () {
+						window.location.replace('/');
+					}, 5000);
+				}
+			});
 	});
 </script>
 
@@ -23,4 +37,5 @@
 	class="bg-white h-screen w-screen dark:bg-acrylic-700 flex items-center justify-center absolute flex-col dark:text-white text-black select-none"
 >
 	<img src="/logo/logo.svg" alt="Hydralite logo" width="8%" class="motion-safe:animate-pulse" />
+	<h1 class="duration-500 text-lg font-bold w-[50%] text-center mt-4">{errorMSG}</h1>
 </div>
